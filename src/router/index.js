9ae858e2971341cc6643c466d440b2e02e24c962
keke <keke@mail.com>
2021-01-03 20:52:23 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Container from '@/components/Container'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Container',
+      component: Container
+    }
+  ]
+})
